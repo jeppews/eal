ed60056b10d40a3b5046a87d3ebd604c14756423
Jeppe Smith <mail@jeppesmith.dk>
2014-10-31 13:46:04 +0100

    header til thomas

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>EAL</title>
+    <link rel="stylesheet" href="/css/frontpage-content.css" media="screen">
+  </head>
+  <body>
+    
+  </body>
+</html>
