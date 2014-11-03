cb86deb9814a3ed080ecae883e4189ac49b25b7e
Jeppe Smith <mail@jeppesmith.dk>
2014-11-03 10:43:29 +0100

    content-boxes færdig

---
+++ content-boxes.php
@@ -0,0 +1,50 @@
+<div class="content-boxes">
+	<div class="content-box">
+		<img src="img/placeholder-content-box.jpg" alt="lol jo">
+		<h1>Videregående uddannelser</h1>
+		<div class="hover">
+			<em>Se hvilke videregående uddannelser vi tilbyder</em>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi harum et quasi, nihil dolorum delectus necessitatibus dolorem veniam porro impedit, quod architecto rem magnam quam nesciunt commodi dicta consequuntur esse.</p>
+			<ul>
+				<li><a href="#">Link uno</a></li>
+				<li><a href="#">Link dós</a></li>
+				<li><a href="#">Link tres</a></li>
+				<li><a href="#">Link quatro</a></li>
+				<li><a href="#">Link cinco</a></li>
+				<li><a href="#">Link seís</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="content-box">
+		<img src="img/placeholder-content-box.jpg" alt="lol jo">
+		<h1>Efteruddannelser</h1>
+		<div class="hover">
+			<em>Se hvilke videregående uddannelser vi tilbyder</em>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi harum et quasi, nihil dolorum delectus necessitatibus dolorem veniam porro impedit, quod architecto rem magnam quam nesciunt commodi dicta consequuntur esse.</p>
+			<ul>
+				<li><a href="#">Link uno</a></li>
+				<li><a href="#">Link dós</a></li>
+				<li><a href="#">Link tres</a></li>
+				<li><a href="#">Link quatro</a></li>
+				<li><a href="#">Link cinco</a></li>
+				<li><a href="#">Link seís</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="content-box">
+		<img src="img/placeholder-content-box.jpg" alt="lol jo">
+		<h1>Kurser</h1>
+		<div class="hover">
+			<em>Se hvilke videregående uddannelser vi tilbyder</em>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi harum et quasi, nihil dolorum delectus necessitatibus dolorem veniam porro impedit, quod architecto rem magnam quam nesciunt commodi dicta consequuntur esse.</p>
+			<ul>
+				<li><a href="#">Link uno</a></li>
+				<li><a href="#">Link dós</a></li>
+				<li><a href="#">Link tres</a></li>
+				<li><a href="#">Link quatro</a></li>
+				<li><a href="#">Link cinco</a></li>
+				<li><a href="#">Link seís</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
--- content.php
+++
@@ -1,5 +0,0 @@
-<?php include 'header.php'; ?>
-
-<main class="frontpage">
-  
-</main>
