b213c645e957202576c88bbbfa0678f4a550033a
Thomas Nielsen <tbnielsen92@hotmail.com>
2014-11-03 10:16:21 +0100

    sass

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>EAL</title>
+    <title>eal.dk - Erhvervsakademiet Lillebælt</title>
     <link rel="stylesheet" href="/css/frontpage-content.css" media="screen">
   </head>
   <body>
